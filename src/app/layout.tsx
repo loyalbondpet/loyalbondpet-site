b8f2286655da8d7857493dd448c500e99b385ff9
loyalbondpet <offroadpart@outlook.com>
2026-08-20 11:02:37 +0800

    perf: defer GA4 to lazyOnload to improve LCP

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -86,10 +86,10 @@ export default function RootLayout({
           Skip to content
         </a>
         <Script
-          strategy="afterInteractive"
+          strategy="lazyOnload"
           src="https://www.googletagmanager.com/gtag/js?id=G-ZDPNEJYXTM"
         />
-        <Script id="google-analytics" strategy="afterInteractive">
+        <Script id="google-analytics" strategy="lazyOnload">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
